41e4db6281a59a7efee53321fcc44e43a96aff33
JiyaKapoor <jiyakapoor1409@gmail.com>
2026-05-25 20:34:45 +0530

    DB query added for alerts and headCnt

--- src/main/java/com/SmartAttendance/demo/Consumer/AnalyticsConsumer.java
+++ src/main/java/com/SmartAttendance/demo/Consumer/AnalyticsConsumer.java
@@ -1,14 +1,23 @@
 package com.SmartAttendance.demo.Consumer;
 
+import com.SmartAttendance.demo.KafkaEvent.SessionClosedEvent;
+import com.SmartAttendance.demo.Repository.ClassRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public class AnalyticsConsumer {
+    @Autowired
+    private ClassRepository classRepository;
     @KafkaListener(topics = "session.closed",
             groupId = "analytics-service")
-    public void consume(){
+    public void consume(SessionClosedEvent sessionClosedEvent){
         //this consumer basically loops over all the students to provide them insights on their attendance trends
+        Long classId=sessionClosedEvent.getClassId();
+        List<Long> enrolledStudents=classRepository.findEnrolledStudentIdsByClassId(classId);
 
     }
 
